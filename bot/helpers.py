639d6760bc6d891211e74696aa366f4ddbe05ee5
dedsec <notdedsec@gmail.com>
2025-07-26 16:32:51 +0530

    add options to avoid rate limits

--- bot/helpers.py
+++ bot/helpers.py
@@ -4,16 +4,34 @@ import requests
 from yt_dlp import YoutubeDL
 
 YTDL_WORKER = os.getenv('YTDL_WORKER')
+PO_TOKEN = os.getenv('PO_TOKEN')
 
-ytdl_options = {
+COOKIES_BROWSER = os.getenv('COOKIES_BROWSER')
+COOKIES_PROFILE = os.getenv('COOKIES_PROFILE')
+
+ytdl = YoutubeDL({
     'format': 'b[filesize<=?10M][filesize_approx<=?10M]',
     'quiet': True,
-}
-ytdl = YoutubeDL(ytdl_options)
+    'no_color': True,
+    'cookiesfrombrowser': (COOKIES_BROWSER, COOKIES_PROFILE),
+    'extractor_args': {'youtube': [f'player-client=default,mweb;po_token=mweb.gvs+{PO_TOKEN}']}
+})
+
+ytdl_fallback = YoutubeDL({
+    'format': 'w',
+    'quiet': True,
+    'no_color': True,
+    'cookiesfrombrowser': (COOKIES_BROWSER, COOKIES_PROFILE),
+    'extractor_args': {'youtube': [f'player-client=default,mweb;po_token=mweb.gvs+{PO_TOKEN}']}
+})
 
 
 def get_info(url: str) -> dict:
-    if YTDL_WORKER:
+    if YTDL_WORKER and any(x in url for x in ['instagram.com', 'tiktok.com']):
         return requests.get(YTDL_WORKER, params={'url': url}).json()
     else:
-        return ytdl.extract_info(url, download=False)
+        try:
+            return ytdl.extract_info(url, download=False)
+        except:
+            return ytdl_fallback.extract_info(url, download=False)
+
